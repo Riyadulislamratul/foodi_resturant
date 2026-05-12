a0d7443af051f0082d60ace77ab96ac64a0f924e
Riyadulislamratul <riadul1810833319@gmail.com>
2026-05-13 00:57:01 +0600

    Choose us section added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import AboutUs from '../components/AboutUs'
+import ChooseUs from '../components/ChooseUs'
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <AboutUs />
+      <ChooseUs />
     </>
   )
 }
